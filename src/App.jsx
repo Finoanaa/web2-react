8774a5eb11404fa22f7be7451aa60207cf5380a2
Yume <hei.hajatiana@gmail.com>
2024-07-02 11:34:38 +0300

    feat: custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,54 @@
 import { useState } from 'react';
 import './App.css'
 
-export default function App() {
-  const [count, updateCount] = useState(0);
-
-  function increment() {
-    if (count < 10) {
-      updateCount(count + 1);
+// custom hook
+function useToggleColor() {
+  const [color, setColor] = useState("red");
+  function toggleColor() {
+    if (color === "red") {
+      setColor("blue");
+    } else {
+      setColor("red");
     }
   }
+  // { color: "red", toggleColor: () => {...} }
+  // { color: "blue", toggleColor: () => {...} }
+  return {color, toggleColor}
+}
 
+function useAlert() {
+  return function() {
+    alert("alert!!!")
+  }
+}
+
+function ColorText() {
+  const {color, toggleColor} = useToggleColor();
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={increment}>increment</button>
-      <button onClick={() => updateCount(count - 1)}>decrement</button>
+    <button onClick={toggleColor}>{color}</button>
+  )
+}
+
+function ToggleColor() {
+  const {color, toggleColor} = useToggleColor();
+  return (
+    <div
+      className={`${color} container`}
+      onClick={toggleColor}
+    >
     </div>
+  )
+}
+
+export default function App() {
+  const triggerAlert = useAlert();
+  return (
+    <>
+      <ColorText />
+      <ToggleColor />
+
+      <button onClick={triggerAlert}>trigger alert</button>
+    </>
   )
 }
 
