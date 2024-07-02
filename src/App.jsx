c6a9fe6b505e967cd7d43cceb5fcf64c0923bb5b
Yume <hei.hajatiana@gmail.com>
2024-07-02 10:54:06 +0300

    feat: props

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,31 @@
-import {useState} from 'react';
 import './App.css'
 
-function useToggleColor(defaultColor = "red") {
-  const [color, setColor] = useState(defaultColor);
-  const toggleColor = () => {
-    if (color === "red") {
-      setColor("blue");
-    } else {
-      setColor("red");
-    }
-  }
-  return {
-    color,
-    toggleColor,
-  }
+function Button(props) {
+  // props: {
+  //  rehefaClicked: () => {}
+  // }
+  return <button onClick={props.rehefaClicked}>{props.text}</button>
 }
 
-function useCounter(defaultCount = 0) {
-  const [count, setCount] = useState(defaultCount);
-
-  const increment = () => {
-    setCount(count + 1);
-  }
-
-  const decrement = () => {
-    setCount(count - 1);
-  }
-
-  return {
-    count,
-    increment,
-    decrement,
-  }
-}
-
-function AnotherComponent() {
-  const {color, toggleColor} = useToggleColor();
-  return <div className={color} onClick={toggleColor}>composant hafa</div>
-}
-
-function App() {
-  const {count, increment, decrement} = useCounter(0);
-
+export default function App() {
   return (
     <div>
-      <div>{count}</div>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
+      <Button
+        text="click1"
+        rehefaClicked={() => {
+          alert("alert!")
+        }}
+      />
+      <Button
+        text="click2"
+        rehefaClicked={() => {
+          console.log('console!');
+        }}
+      />
     </div>
   )
 }
 
-export default App
+// const button = document.getElementById("btn")
+// button.onclick = () => {
+// }
