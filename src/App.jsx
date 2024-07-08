bd14f926149cea25920c6328ab4fab47306ea3a6
Yume <hei.hajatiana@gmail.com>
2024-07-08 16:38:14 +0300

    feat: without useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import { useEffect, useState } from "react";
+import {useState} from "react";
+
+function validate(s) {
+  if (!s.length) {
+    return false
+  }
+  else if (/[0-9]/.test(s)) {
+    return false;
+  }
+  return true;
+}
+
 
 export default function App() {
   const [inputValue, setInputValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  useEffect(() => {
-    if (!inputValue.length) {
-      setIsValid(false);
-    }
-    else if (/[0-9]/.test(inputValue)) {
-      setIsValid(false);
-    }
-    else {
-      setIsValid(true);
-    }
-  }, [inputValue])
+  function handleInputValueChange(ev) {
+    const {value} = ev.target;
+    setIsValid(validate(value));
+    setInputValue(value)
+  }
 
   return (
     <div>
       <input
-        onChange={ev => setInputValue(ev.target.value)}
+        onChange={handleInputValueChange}
         value={inputValue}
       />
       {isValid ? "" : "invalid"}
