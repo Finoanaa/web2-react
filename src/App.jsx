dec863c4e0d52a7fffb6f874348a620778a44cc6
Yume <hei.hajatiana@gmail.com>
2024-07-29 15:13:17 +0300

    feat: getting started with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,72 @@
-import {useState} from "react";
-import {useFetch} from "./useFetch";
+import {createBrowserRouter, RouterProvider, Link} from "react-router-dom";
+import Ohatra from "./Ohatra";
+
+const Menu = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/">home</Link>
+      </li>
+
+      <li>
+        <Link to="/ohatra">ohatra</Link>
+      </li>
+
+      <li>
+        <Link to="/eto-ambony-eto">eto ambony eto</Link>
+      </li>
+
+      <li>
+        <Link to="klsdjfklsdjfk">tsy aiko</Link>
+      </li>
+
+    </ul>
+  )
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        Home
+
+        <Menu />
+      </div>
+    )
+  },
+  {
+    path: "/ohatra",
+    element: (
+      <div>
+        Ohatra
+        <Menu />
+      </div>
+    )
+  },
+  {
+    path: "/eto-ambony-eto",
+    element: (
+      <div>
+        Eto ambony eto
+        <Menu />
+      </div>
+
+    )
+  },
+  {
+    path: "*",
+    element: (
+      <div>
+        <h1>tsy hita</h1>
+        <Link to="/">go back home</Link>
+      </div>
+    )
+  }
+])
 
 
 export default function App() {
-  const [resource, setResource] = useState("users");
-  const [data, {isLoading}] = useFetch(resource);
-
   return (
-    <div>
-      <div>
-        <select onChange={ev => setResource(ev.target.value)}>
-          <option value="users">users</option>
-          <option value="posts">posts</option>
-        </select>
-
-        <h1>{resource}</h1>
-        <ul>
-          {isLoading ? "loading..." : ""}
-
-          {data.map(entry => (
-            <li>
-              {entry.name || entry.title}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
+      <RouterProvider router={router} />
+  );
 }
---
+++ src/Ohatra.jsx
@@ -0,0 +1,3 @@
+export default function Ohatra() {
+  return <h1>ohatra</h1>
+}
