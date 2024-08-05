73f07acfc51f69d116511563b3a8121ee3b8e8c2
Yume <hei.hajatiana@gmail.com>
2024-08-05 16:34:20 +0300

    feat: useEffect review

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,33 @@
-import {Navigate, createBrowserRouter, RouterProvider, Link, Outlet} from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const NotFound = () => {
-  return (
-    <h1>very</h1>
-  )
+// lifecycle of a React component:
+// - mounted (useEffect with empty deps)
+// - updated (useEffect with deps)
+// - unmounted (useEffect.effect return)
+
+function Anakiray() {
+  // allocate mem
+  useEffect(() => {
+
+    // cleanup function
+    return () => {
+      // free mem
+    }
+  }, []);
+
+  return <p>oke ah</p>;
 }
-
-const Layout = () => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="home">Home</Link>
-        </li>
-        <li>
-          <Link to="ohatra">Ohatra</Link>
-        </li>
-      </ul>
-
-      <div>
-        <Outlet />
-      </div>
-    </div>
-  )
-}
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
-
-const Ohatra = () => {
-  return (
-    <div>
-      <h1>Ohatra</h1>
-      <Link to="/">back home</Link>
-    </div>
-  )
-}
-
-const ROUTER = createBrowserRouter([
-  {
-    path: "/app",
-    element: <Layout />,
-    children: [
-      {
-        // route rehefa "/app" fotsiny
-        index: true,
-        element: <Navigate to="home" />,
-      },
-      {
-        // "/app/home"
-        path: "home",
-        element: <Home />
-      },
-      {
-        // "/app/ohatra"
-        path: "ohatra",
-        element: <Ohatra />
-      },
-      {
-        path: "*",
-        element: <h1>tsy misy</h1>
-      }
-    ]
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  }
-]);
-
 
 export default function App() {
+  const [isShown, setIsShown] = useState(false);
+
   return (
-    <RouterProvider router={ROUTER} />
+    <div>
+      {isShown ?  <Anakiray /> : undefined}
+
+      <button onClick={() => {
+        setIsShown(!isShown)
+      }}>toggle hidden</button>
+    </div>
   )
 }
